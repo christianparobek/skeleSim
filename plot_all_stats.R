6a3ec80800b76cf530552b07dc46e537a8731369
smhoban <shoban@alumni.nd.edu>
2015-03-20 09:41:40 -0400

    changes to plottting function, before running test with more synthetic data

--- plot_all_stats.R
+++ plot_all_stats.R
@@ -6,14 +6,16 @@ plot_all_stats<-function(main_list){
   
   #in the future this will be the real results dataframe
   results_df<-main_list$results_from_analyses
-  
-  #but for now we will make one up, assuming TWO scenarios
   num_scen<-length(main_list$scenarios_list[,1])
-  #results_df<-data.frame(matrix(NA,nrow=main_list$common_params$num_reps*num_scen,ncol=3))
-  #here I will put the names of the functions/summ statistics that get called
-  stats_names<-c("alleles","het","fis","gst")
-  
-  #these results are totally synthetic
+  stats_names<-names(main_list$results_from_analyses)
+    
+#   but for now we will make one up, assuming TWO scenarios
+#   num_scen<-2
+#   results_df<-data.frame(matrix(NA,nrow=main_list$common_params$num_reps*num_scen,ncol=3))
+#   here I will put the names of the functions/summ statistics that get called
+#   stats_names<-c("alleles","het","fis","gst")
+#   
+#   these results are totally synthetic
 #   results_df[,1]<-c(rep(1,main_list$common_params$num_reps),rep(2,main_list$common_params$num_reps))
 #   results_df[,2]<-abs(floor(rnorm(main_list$common_params$num_reps*num_scen,1,10)))
 #   results_df[,3]<-runif(main_list$common_params$num_reps*num_scen)
