64b9412ec9858373d253c15669cd88be098ffb78
Sean <shoban@alumni.nd.edu>
2015-06-12 11:11:45 -0400

    Update rms.classes.R

--- rmetasim/rms.classes.R
+++ rmetasim/rms.classes.R
@@ -4,33 +4,37 @@ setClassUnion("charOrNULL", c("character", "NULL"))
 setClassUnion("intOrNum", c("integer","numeric", "NULL"))
 setClassUnion("funcOrNULL", c("function", "NULL"))
 setClassUnion("posixOrNULL", c("POSIXct", "POSIXlt", "NULL"))
-setClassUnion("matrOrVect", c("matrix", "vector", "NULL"))
 
 #' @title rmetasim Parameters Class
 #' @description An S4 class storing parameters specific to rmetasim
 #'
 #' @rdname rmetasim.classes
 #'
-#' @slot num.stg number of stages
-#' @slot self.rate selfing rate
-#' @slot surv.matr survival matrix
-#' @slot repr.matr reproductive matrix (females)
-#' @slot male.matr male contribution matrix (males)
-#' @slot carrying carrying capacity
-#' @slot init.pop.sizes initial starting population census
-#' @slot num.alleles number of alleles per locus
-#' @slot allele.freq initial allele frequencies
-#' @slot num.gen number of generations to run
+#' @slot num.stg a single integer for number of stages
+#' @slot self.rate a single numeric for selfing rate
+#' @slot surv.matr a matrix of numerics for survival transitions between stages
+#' @slot repr.matr a matrix of integers for reproductive (females)
+#' @slot male.matr a matrix of integers for male contribution (males)
+#' @slot carrying a single integer for carrying capacity
+#' @slot init.pop.sizes a vector of integers for initial starting population census sizes (length number of pop'ns)
+#' @slot num.alleles a vector of integers for number of alleles per locus (length number of loci)
+#' @slot allele.freq a vector of numerics for initial allele frequencies ???
+#' @slot num.gen a single integer of number of generations to run
 #'
 setClass(
   Class = "fastsimcoal.params",
   slots = c(num.stg = "intOrNum", self.rate = "intOrNum",
-            surv.matr = "matrOrVect", repr.matr = "matrOrVect",
-            male.matr = "matrOrVect", carrying = 
-  
+            surv.matr = "intOrNum", repr.matr = "intOrNum",
+            male.matr = "intOrNum", carrying = "intOrNum",
+            init.pop.sizes = "intOrNum", num.alleles = "intOrNum",
+            
+            num.gen = "intOrNum"
   ),
   prototype = c(num.stg = NULL, self.rate = NULL,
-  
-  
+            surv.matr = NULL, repr.matr = NULL,
+            male.matr = NULL, carrying = NULL,
+            init.pop.sizes = NULL, num.alleles = NULL,
+            
+            num.gen = NULL
   )
 )
