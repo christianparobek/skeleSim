b8ddff76779b141f264b506eef5394e753453a35
Sean <shoban@alumni.nd.edu>
2015-06-12 12:07:09 -0400

    Update rms.init.landscape.R

--- rmetasim/rms.init.landscape.R
+++ rmetasim/rms.init.landscape.R
@@ -1,1 +1,10 @@
+# initiate an rmetasim landscape with parameters
 
+init.landscape <- function(num.pops = NULL, carrying = NULL, sample.size = NULL,
+                            mig.rates = NULL, num.loc = NULL, ) {
+ 
+
+
+
+
+}
