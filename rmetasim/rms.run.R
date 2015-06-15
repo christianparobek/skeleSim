6f71878d5fafc3eeeec5ec7ed3e00d2705ef3844
Sean <shoban@alumni.nd.edu>
2015-06-15 12:28:09 -0400

    Update rms.run.R

--- rmetasim/rms.run.R
+++ rmetasim/rms.run.R
@@ -3,19 +3,19 @@ rms.run<-function(params){
   
   #create rmetasim landscape skeletonland
   skeleland<-rms.init.landscape(
-      num.pops = sc@num.pops, carrying = sc@pop.size, sample.size = sc@sample.size, mig.rates = sc@migration, 
+      num.pops = sc@num.pops, carrying = sc@pop.size, mig.rates = sc@migration, 
       num.loc = sc@num.loci, loc.type = sc@locus.type, mut.rate = sc@mut.rate, seq.length = sc@sequence.length, 
       num.stgs = sc@simulator.params@num.stgs, selfing = sc@simulator.params@selfing, 
       surv.matr = sc@simulator.params@surv.matr, repr.matr = sc@simulator.params@repr.matr, 
       male.matr = sc@simulator.params@male.matr,
-      init.pop.sizes = sc@simulator.params@init.pop.sizes, num.gen = sc@simulator.params@num.gen, 
+      init.pop.sizes = sc@simulator.params@init.pop.sizes, 
       num.alleles = sc@simulator.params@num.alleles, allele.freqs = sc@simulator.params@allele.freqs)
   
   #run a number of generations
-  skeleland<-landscape.simulate(skeleland, num.gen)
+  skeleland<-landscape.simulate(skeleland, num.gen = sc@simulator.params@num.gen)
 
   #take samples
-  skeleland_samp<-landscape.sample(skeleland, sample.size)
+  skeleland_samp<-landscape.sample(skeleland, sample.size = sc@sample.size)
   
   #now store the results
   params@rep.sample<- rms.convert(skeleland_samp)
