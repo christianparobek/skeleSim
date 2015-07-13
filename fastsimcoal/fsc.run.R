1e2ea8d03fb7011cc1775a8f7703fd616f2307ea
Sean <shoban@alumni.nd.edu>
2015-07-13 18:23:04 -0400

    Moving fsc scenario checks to its own file

--- fastsimcoal/fsc.run.R
+++ fastsimcoal/fsc.run.R
@@ -40,21 +40,3 @@ fsc.run <- function(params) {
   params@rep.sample <- fsc.read(arp.file, params)
   params
 }
-
-fsc.scenarioCheck <- function(params) {
-  # check that sample times and growth rates are of length number of populations, and
-  #   that historical events matrix converges 
-  results <- sapply(params@scenarios, function(sc) {
-    
-    c(
-      samptime.eq.npops = length(sc@simulator.params@sample.times) == sc@num.pops,
-      growth.eq.npops = length(sc@simulator.params@growth.rate) == sc@num.pops,
-      hist.ev.good = fsc.histEvConverges(
-                            sc@simulator.params@hist.ev,
-                            sc@pop.size,
-                            sc@simulator.params@growth.rate,
-                            length(sc@migration))
-    )
-  })
-  return(results)
-}
