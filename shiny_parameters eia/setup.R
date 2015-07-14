2d27001bb049551ab2590e6f63e9edc95389dee9
Eric Archer <eric.archer@noaa.gov>
2015-07-14 16:18:30 -0400

    added analyses.requested slot playing with Shiny

---
+++ shiny_parameters eia/setup.R
@@ -0,0 +1,12 @@
+#
+# load some packages
+# source some files
+#
+options(shiny.trace = F)  # cahnge to T for trace
+require(shiny)
+require(igraph)
+
+source("utils.R")
+source("mig.matrix.R")
+source("simcoal-history.R")
+source("../skeleSim.classes.R")
