ea56312c6bc99af94d9c0fb084629c50e0a609f5
Eric Archer <eric.archer@noaa.gov>
2015-07-16 21:13:57 -0400

    fixed sim run to write proper script

--- shiny_parameters/runSim.R
+++ shiny_parameters/runSim.R
@@ -4,21 +4,22 @@ observeEvent(input$btnRunSim, {
   if(!is.null(fnameLabel)) {
     scriptFname <- paste(fnameLabel, ".script.R", sep = "")
     paramFname <- paste(fnameLabel, ".rdata", sep = "")
-    outFname <- paste(fnameLabel, ".skeleSim.out.rdata", sep = "")
+    resultFname <- paste(fnameLabel, ".result.rdata", sep = "")
     write("rm(list = ls())", file = scriptFname)
+    #write("library(skeleSim)", file = scriptFname, append = TRUE)
     write("library(methods)", file = scriptFname, append = TRUE)
-    paramFname <- "test.ssClass.rdata"
     line <- paste("load('", paramFname, "')", sep = "")
     write(line, file = scriptFname, append = TRUE)
-    write("ssClass <- runSim(ssClass)", file = scriptFname, append = TRUE)
-    write("ssClass@timing <- NULL", file = scriptFname, append = TRUE)
-    line <- paste("save(ssClass, file = '", outFname, "')", sep = "")
+    result <- paste(objLabel, ".result", sep = "")
+    line <- paste(result, " <- runSim(", objLabel, ")", sep = "")
+    write(line, file = scriptFname, append = TRUE)
+    line <- paste("save(", result, ", file = '", resultFname, "')", sep = "")
     write(line, file = scriptFname, append = TRUE)
 
     cond <- TRUE
     output$txtRunStatus <- renderText({
       if(cond) {
-        cmd <- paste("nohup R CMD BATCH --no-restore", scriptFname)
+        cmd <- paste("nohup R CMD BATCH --no-restore", resultFname)
         system(cmd, wait = FALSE)
         "Simulation run commenced."
         fnameLabel <<- NULL
