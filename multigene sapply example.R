8c7622e1a1bade3e4e86d0de6d95c29b99366cb1
Eric Archer <eric.archer@noaa.gov>
2015-07-23 17:28:29 -0700

    example of sapply-ing over multigene gtypes object

---
+++ multigene sapply example.R
@@ -0,0 +1,13 @@
+library(strataGdevel)
+rm(list = ls())
+
+data(woodmouse)
+genes <- list(gene1=woodmouse[,1:500], gene2=woodmouse[,501:965])
+x <- new("multidna", genes)
+x.g <- sequence2gtypes(x)
+strata(x.g) <- c("A", "B")
+
+phist <- sapply(locNames(x.g), function(n) {
+  unname(statPhist(x.g[, n, ]))
+})
+
