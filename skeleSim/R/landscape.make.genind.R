f5ea0fc585b991857fa7792139f991ba9c7d8bc7
Allan Strand <stranda@gmail.com>
2015-12-08 21:09:44 +0100

    changes to the code to convert landscapes to genind

--- skeleSim/R/landscape.make.genind.R
+++ skeleSim/R/landscape.make.genind.R
@@ -2,23 +2,22 @@
 #' @description Create genind object from landscape
 #'
 #' @param l a \code{rmetasim} landscape object.
-#' @param popnames character vector of population names.
 #'
 #' @importFrom adegenet genind
 #'
 #' @export
 #' 
-landscape.make.genind <- function(Rland,popnames=NULL)
+landscape.make.genind <- function(l)
   {
-      tab <- 2*landscape.ind.freq(Rland)
-      dimnames(tab) <- list(rownames=1:dim(tab)[1],colnames=landscape.freq.locnames(Rland))
+      tab <- 2*landscape.ind.freq(l)
+      dimnames(tab) <- list(rownames=1:dim(tab)[1],colnames=landscape.freq.locnames(l))
       if (is.null(popnames))
           {
-              populations <- landscape.populations(Rland)
+              populations <- landscape.populations(l)
           }
       else
           {
               populations <- popnames
           }
-      adegenet::genind(tab,pop=as.factor(populations),ploidy=2)
+      genind(tab,pop=as.factor(populations),ploidy=2)
   }
