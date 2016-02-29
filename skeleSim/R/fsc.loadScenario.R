e109e49f7f1b7a12886f854f425ee55b46e6d4d6
Eric Archer <eric.archer@noaa.gov>
2016-02-29 06:15:22 -0800

    merged hist.ev arguments into fsc.loadScenario

--- skeleSim/R/fsc.loadScenario.R
+++ skeleSim/R/fsc.loadScenario.R
@@ -15,7 +15,6 @@
 #' @param sample.times a vector giving the number of generations in the past
 #'   at which samples are taken.
 #' @param growth.rate a vector giving the growth rate of each population.
-#' @param hist.ev a matrix describing historical events.
 #' @param num.chrom a value giving the number of chromosomes that the
 #'   \code{locus.params} marker specifications should be copied for. If
 #'   \code{NULL}, then chromosome assignment is taken from the
@@ -34,6 +33,17 @@
 #'   alleles allowed). A value of 0 means no range constraint.
 #' @param fastsimcoal.exec executable
 #'
+#' @param num.gen Number of generations, t, before present at which the
+#'   historical event happened.
+#' @param source.deme Source deme (the first listed deme has index 0)
+#' @param sink.deme Sink deme
+#' @param prop.migrants Expected proportion of migrants to move from source to sink.
+#' @param new.sink.size New size for the sink deme, relative to its size at
+#'   generation t.
+#' @param new.sink.growth New growth rate for the sink deme.
+#' @param new.mig.mat New migration matrix to be used further back in time.
+#'
+#'
 #' @note Vectors for \code{pop.size, sample.size, sample.times, and growth.rate}
 #'   will be expanded/recycled to ensure they are as long as \code{num.pops}.
 #'
@@ -48,11 +58,13 @@
 #'
 fsc.loadScenario <- function(
   num.pops, pop.size, sample.size, mut.rate, migration = NULL,
-  sample.times = NULL, growth.rate = NULL, hist.ev = NULL,
+  sample.times = NULL, growth.rate = NULL,
   locus.type = c("dna", "msat", "snp"), sequence.length = NULL,
   num.loci = NULL, transition.rate = NULL, gsm.param = NULL,
   range.constraint = NULL, recomb.rate = NULL, chromosome = NULL,
-  num.chrom = NULL, fastsimcoal.exec = "fsc252") {
+  num.chrom = NULL, num.gen = NULL, source.deme = NULL, sink.deme = NULL,
+  prop.migrants = NULL, new.sink.size = NULL, new.sink.growth = NULL,
+  new.mig.mat = NULL, fastsimcoal.exec = "fsc252") {
 
   # convert NULLs to default values (some may not be used)
   if(is.null(num.loci)) num.loci <- 1
@@ -63,6 +75,13 @@ fsc.loadScenario <- function(
   if(is.null(range.constraint)) range.constraint <- 0
   if(is.null(recomb.rate)) recomb.rate <- 0
   if(is.null(chromosome)) chromosome <- 1
+  if(is.null(num.gen)) num.gen <- 0
+  if(is.null(source.deme)) source.deme <- 0
+  if(is.null(sink.deme)) sink.deme <- 0
+  if(is.null(prop.migrants)) prop.migrants <- 1
+  if(is.null(new.sink.size)) new.sink.size <- 1
+  if(is.null(new.sink.growth)) new.sink.growth <- 0
+  if(is.null(new.mig.mat)) new.mig.mat <- 0
 
   pop.size <- rep(pop.size, length.out = num.pops)
   sample.size <- rep(sample.size, length.out = num.pops)
@@ -91,7 +110,11 @@ fsc.loadScenario <- function(
     sample.times = rep(sample.times, length.out = num.pops),
     growth.rate = rep(growth.rate, length.out = num.pops)
   )
-  fsc@hist.ev <- hist.ev
+  fsc@hist.ev <- fscHistEv(
+    num.gen = num.gen, source.deme = source.deme, sink.deme = sink.deme,
+    prop.migrants = prop.migrants, new.sink.size = new.sink.size,
+    new.sink.growth = new.sink.growth, new.mig.mat = new.mig.mat
+  )
   fsc@locus.params <- fscLocusParams(
     locus.type = locus.type,
     sequence.length = sequence.length,
--- test_code/testRunFastsimcoal.R
+++ test_code/testRunFastsimcoal.R
@@ -31,13 +31,11 @@ test.params@scenarios <- list(
     migration = matrix(
       c(0, 0.01, 0.05, 0.025, 0, 0.025, 0.05, 0.01, 0), nrow = 3
     ),
-    hist.ev = fscHistEv(
-      num.gen = c(2000, 2980, 3000, 15000),
-      source.deme = c(1, 1, 1, 0),
-      sink.deme = c(2, 1, 0, 2),
-      prop.migrants = c(0.05, 0, 1, 1),
-      new.sink.size = c(1, 0.04, 1, 3)
-    )
+    num.gen = c(2000, 2980, 3000, 15000),
+    source.deme = c(1, 1, 1, 0),
+    sink.deme = c(2, 1, 0, 2),
+    prop.migrants = c(0.05, 0, 1, 1),
+    new.sink.size = c(1, 0.04, 1, 3)
   )
 )
 
