7164fbdedf481df9a1b7746e7a4b7619ca818105
allan strand <stranda@gmail.com>
2016-04-15 16:40:42 -0400

    same bug

--- skeleSim/inst/shiny/skeleSimShinyGUI/simcoal-server.R
+++ skeleSim/inst/shiny/skeleSimShinyGUI/simcoal-server.R
@@ -223,7 +223,7 @@ output$simexec <- renderUI({
                         sim.exec <- unique(sim.exec)
                         sim.exec <- basename(Sys.which(sim.exec))
                         sim.exec <- sim.exec[nchar(sim.exec)>0]
-                        sim.exec <- sort(sim.exec)
+                        sim.exec <- sort(sim.exec,decreasing=TRUE)
                         ui <- selectInput("fscexec","Select fastsimcoal executable",choices=sim.exec)
                     }
     ui
