4b09609551b2ddc29b126bcd3cf720d9e210ba98
allan strand <stranda@gmail.com>
2016-05-18 14:47:57 -0400

    set up to launch application in new browser

--- skeleSim/R/skeleSimGUI.R
+++ skeleSim/R/skeleSimGUI.R
@@ -7,10 +7,10 @@
 #' @importFrom igraph graph.adjacency plot.igraph
 #' @export
 #' 
-skeleSimGUI <- function() {
+skeleSimGUI <- function(launch.browser=TRUE) {
   appDir <- system.file("shiny", "skeleSimShinyGUI", package = "skeleSim")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `skeleSim`.", call. = FALSE)
   }
-  runApp(appDir, display.mode = "normal")
+  runApp(appDir, display.mode = "normal", launch.browser=launch.browser)
 }
