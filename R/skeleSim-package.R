7acfc8911645d74ca5f6e1ceb4731cd75526c5ee
Eric Archer <eric.archer@noaa.gov>
2016-05-24 09:35:51 -0700

    sample data temporarily moved to Archive

---
+++ R/skeleSim-package.R
@@ -0,0 +1,44 @@
+#' skeleSim
+#'
+#' @docType package
+#' @name skeleSim-package
+#' @aliases skeleSim
+#' @title Genetic simulation engine
+#' @keywords package
+NULL
+#'
+#' #' @docType data
+#' #' @name ssc.fscSEQ
+#' #' @title Sample fastsimcoal sequence skeleSim parameters object
+#' #' @description Sample fastsimcoal sequence skeleSim parameters object
+#' #' @usage data(ssc.fscSEQ)
+#' #' @format skeleSim.class
+#' #' @keywords datasets
+#' NULL
+#'
+#' #' @docType data
+#' #' @name ssc.fscSSR
+#' #' @title Sample fastsimcoal SSR skeleSim parameters object
+#' #' @description Sample fastsimcoal SSR skeleSim parameters object
+#' #' @usage data(ssc.fscSSR)
+#' #' @format skeleSim.class
+#' #' @keywords datasets
+#' NULL
+#'
+#' #' @docType data
+#' #' @name ssc.rmsSEQ
+#' #' @title Sample rmetasim sequence skeleSim parameters object
+#' #' @description Sample rmetasim sequence skeleSim parameters object
+#' #' @usage data(ssc.rmsSEQ)
+#' #' @format skeleSim.class
+#' #' @keywords datasets
+#' NULL
+#'
+#' #' @docType data
+#' #' @name ssc.rmsSSR
+#' #' @title Sample rmetasim SSR skeleSim parameters object
+#' #' @description Sample fastsimcoal sequence skeleSim parameters object
+#' #' @usage data(ssc.rmsSSR)
+#' #' @format skeleSim.class
+#' #' @keywords datasets
+#' NULL
