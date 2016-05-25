edb66fed5c23ba125873db3f8cc0237f6420ed80
Eric Archer <eric.archer@noaa.gov>
2016-05-25 14:53:51 -0700

    added growth.rate and sample.times back to fastsimcoal.params

--- R/fsc.classes.R
+++ R/fsc.classes.R
@@ -13,6 +13,10 @@ setClassUnion("intOrNULL", c("integer", "NULL"))
 #'   \code{\link{fscHistEv}} function.
 #' @slot locus.params data.frame specifying loci to simulate created by the
 #'   \code{\link{fscLocusParams}} function.
+#' @slot growth.rate a vector \code{num.pop} long giving the growth rate of
+#'   each population.
+#' @slot sample.times a vector \code{num.pop} long giving the time (in generations)
+#'   that samples are drawn from in population.
 #'
 #' @export
 #'
@@ -20,11 +24,13 @@ fastsimcoal.params <- setClass(
   Class = "fastsimcoal.params",
   slots = c(
     fastsimcoal.exec = "character", pop.info = "matrOrNULL",
-    hist.ev = "matrOrNULL", locus.params = "dfOrNULL"
+    hist.ev = "matrOrNULL", locus.params = "dfOrNULL",
+    growth.rate = "intOrNum", sample.times = "intOrNum"
   ),
   prototype = list(
-    fastsimcoal.exec = "fsc252", pop.info = NULL, hist.ev = NULL,
-    locus.params = NULL
+    fastsimcoal.exec = "fsc252", pop.info = NULL,
+    hist.ev = NULL, locus.params = NULL,
+    growth.rate = NULL, sample.times = NULL
   ),
   validity = function(object) {
     return(TRUE)
